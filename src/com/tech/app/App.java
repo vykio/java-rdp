96255cba1c395cef30c38d0ae6321fa47be00829
Gauthier Leurette <leurette.gauthier@gmail.com>
2021-02-22 19:39:10 +0100

    Ajout du début de la barre d'outils
    Mise en place de la barre d'outils (pour l'instant il n'y a que l'onglet "Fichier" et seul la fonction "Exit" fonctionne)
    
    -A voir où placer le code pour la barre d'outil ?
          - La laisser dans la classe mainWindow ou alors créer une classe pour les barres d'outils
    
    - A ajouter : les fonctions sur les différentes options de sous-menu "File"
    
    -A ajouter : La deuxième barre d'outil pour le dessin des RdP
    
    -Problème : Icônes qui ne s'affichent pas -> essayer de les redimensionner car les images sont surement trop grandes

--- src/com/tech/app/App.java
+++ src/com/tech/app/App.java
@@ -7,17 +7,25 @@ package com.tech.app;
 
 import com.tech.app.windows.MainWindow;
 
+import java.awt.*;
+
 public class App implements Runnable {
     @Override
     public void run() {
+
         MainWindow mainWindow = new MainWindow(500,500);
+
+        EventQueue.invokeLater(() -> {
+
+            mainWindow.createMenuBar();
+            mainWindow.setVisible(true);
+        });
+
     }
 
     public synchronized void start() {
         new Thread(this).start();
     }
 
-    public static void main(String[] args) {
-        new App().start();
-    }
+    public static void main(String[] args) {   new App().start();    }
 }
--- src/com/tech/app/windows/MainWindow.java
+++ src/com/tech/app/windows/MainWindow.java
@@ -3,6 +3,14 @@ package com.tech.app.windows;
 import com.tech.app.windows.handlers.MainWindowHandler;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.EventQueue;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainWindow extends Window {
 
@@ -11,6 +19,48 @@ public class MainWindow extends Window {
         setWindowHandler(new MainWindowHandler(this));
 
         build();
+    }
+
+    public void createMenuBar() {
+
+        var menuBar = new JMenuBar();
+
+        var iconNew = new ImageIcon("src/resources/new.png");
+        var iconOpen = new ImageIcon("src/resources/open.png");
+        var iconSave = new ImageIcon("src/resources/save.png");
+        var iconExit = new ImageIcon("src/resources/exit.png");
+
+        var fileMenu = new JMenu("File");
+        var impMenu = new JMenu("Import");
+
+        var newsMenuItem = new JMenuItem("Import newsfeed list...");
+        var bookmarksMenuItem = new JMenuItem("Import bookmarks...");
+        var importMailMenuItem = new JMenuItem("Import mail...");
+
+        impMenu.add(newsMenuItem);
+        impMenu.add(bookmarksMenuItem);
+        impMenu.add(importMailMenuItem);
+
+        var newMenuItem = new JMenuItem("New", iconNew);
+        var openMenuItem = new JMenuItem("Open", iconOpen);
+        var saveMenuItem = new JMenuItem("Save", iconSave);
+
+        var exitMenuItem = new JMenuItem("Exit", iconExit);
+        exitMenuItem.setToolTipText("Exit application");
+
+        exitMenuItem.addActionListener((event) -> System.exit(0));
+
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(impMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
     }
 
     protected void build() {
