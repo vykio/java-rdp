460e3966931d39b12f42d9ccea3c555402f71447
Gauthier Leurette <leurette.gauthier@gmail.com>
2021-02-24 19:12:03 +0100

    Toolbar #3
    Gestion des icônes (améliorable)
    Gestion des évènements liés aux actions sur les boutons "Open" et "Save As"
    -Open -> fenêtre de dialoque, à voir comment ça réagit lorsque l'on aura un fichier à ouvrir,
    -Save As -> fenêtre de dialoque, pas encore de gestion des extensions, pour l'instant, il suffit de donner un nom au fichier avec .txt

--- src/com/tech/app/windows/Toolbar.java
+++ src/com/tech/app/windows/Toolbar.java
@@ -3,16 +3,12 @@ package com.tech.app.windows;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
 
 public class Toolbar extends JFrame {
@@ -42,16 +38,6 @@ public class Toolbar extends JFrame {
         mnuFile.setMnemonic('F');
 
         JMenuItem mnuNewFile = new JMenuItem("New File");
-
-        Image imageNew = null;
-        try {
-            imageNew = ImageIO.read(getClass().getResource("/icons/new.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert imageNew != null;
-        mnuNewFile.setIcon(new ImageIcon(imageNew));
-
         mnuNewFile.setMnemonic('N');
         mnuNewFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
         mnuNewFile.addActionListener(this::mnuNewListener);
@@ -60,26 +46,28 @@ public class Toolbar extends JFrame {
         mnuFile.addSeparator();
 
         JMenuItem mnuOpenFile = new JMenuItem("Open File ...");
-        mnuOpenFile.setIcon(new ImageIcon("icons/open.png"));
+        //mnuOpenFile.setIcon(new ImageIcon("icons/open.png"));
         mnuOpenFile.setMnemonic('O');
         mnuOpenFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+        mnuOpenFile.addActionListener(this::mnuOpenListener);
         mnuFile.add(mnuOpenFile);
 
         JMenuItem mnuSaveFile = new JMenuItem("Save File ...");
-        mnuSaveFile.setIcon(new ImageIcon("icons/save.png"));
+        //mnuSaveFile.setIcon(new ImageIcon("icons/save.png"));
         mnuSaveFile.setMnemonic('S');
         mnuSaveFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
         mnuFile.add(mnuSaveFile);
 
         JMenuItem mnuSaveFileAs = new JMenuItem("Save File As ...");
-        mnuSaveFileAs.setIcon(new ImageIcon("icons/save_as.png"));
+        //mnuSaveFileAs.setIcon(new ImageIcon("icons/save_as.png"));
         mnuSaveFileAs.setMnemonic('A');
+        mnuSaveFileAs.addActionListener(this::mnuSaveAsListener);
         mnuFile.add(mnuSaveFileAs);
 
         mnuFile.addSeparator();
 
         JMenuItem mnuExit = new JMenuItem("Exit");
-        mnuExit.setIcon(new ImageIcon("icons/exit.png"));
+        //mnuExit.setIcon(new ImageIcon("icons/exit.png"));
         mnuExit.setMnemonic('x');
         mnuExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.ALT_DOWN_MASK));
         mnuFile.add(mnuExit);
@@ -91,13 +79,13 @@ public class Toolbar extends JFrame {
         mnuEdit.setMnemonic('E');
 
         JMenuItem mnuUndo = new JMenuItem("Undo");
-        mnuUndo.setIcon(new ImageIcon("icons/undo.png"));
+        //mnuUndo.setIcon(new ImageIcon("icons/undo.png"));
         mnuUndo.setMnemonic('U');
         mnuUndo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK));
         mnuEdit.add(mnuUndo);
 
         JMenuItem mnuRedo = new JMenuItem("Redo");
-        mnuRedo.setIcon(new ImageIcon("icons/redo.png"));
+        //mnuRedo.setIcon(new ImageIcon("icons/redo.png"));
         mnuRedo.setMnemonic('R');
         mnuRedo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, KeyEvent.CTRL_DOWN_MASK));
         mnuEdit.add(mnuRedo);
@@ -105,19 +93,19 @@ public class Toolbar extends JFrame {
         mnuEdit.addSeparator();
 
         JMenuItem mnuCopy = new JMenuItem("Copy");
-        mnuCopy.setIcon(new ImageIcon("icons/copy.png"));
+        //mnuCopy.setIcon(new ImageIcon("icons/copy.png"));
         mnuCopy.setMnemonic('C');
         mnuCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
         mnuEdit.add(mnuCopy);
 
         JMenuItem mnuCut = new JMenuItem("Cut");
-        mnuCut.setIcon(new ImageIcon("icons/cut.png"));
+        //mnuCut.setIcon(new ImageIcon("icons/cut.png"));
         mnuCut.setMnemonic('t');
         mnuCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK));
         mnuEdit.add(mnuCut);
 
         JMenuItem mnuPaste = new JMenuItem("Paste");
-        mnuPaste.setIcon(new ImageIcon("icons/paste.png"));
+        //mnuPaste.setIcon(new ImageIcon("icons/paste.png"));
         mnuPaste.setMnemonic('P');
         mnuPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
         mnuEdit.add(mnuPaste);
@@ -164,6 +152,58 @@ public class Toolbar extends JFrame {
 
         toolbar.add(mnuHelp);
 
+        //Gestion des icônes => améliorable !
+        Image imageNew = null;
+        Image imageOpen = null;
+        Image imageSave = null;
+        Image imageSaveAs = null;
+        Image imageExit = null;
+        Image imageUndo = null;
+        Image imageRedo = null;
+        Image imageCopy = null;
+        Image imagePaste = null;
+        Image imageCut = null;
+        Image imageAbout = null;
+        try {
+            imageNew = ImageIO.read(getClass().getResource("/icons/new.png"));
+            imageOpen = ImageIO.read(getClass().getResource("/icons/open.png"));
+            imageSave = ImageIO.read(getClass().getResource("/icons/save.png"));
+            imageSaveAs = ImageIO.read(getClass().getResource("/icons/save_as.png"));
+            imageExit = ImageIO.read(getClass().getResource("/icons/exit.png"));
+            imageUndo = ImageIO.read(getClass().getResource("/icons/undo.png"));
+            imageRedo = ImageIO.read(getClass().getResource("/icons/redo.png"));
+            imageCopy = ImageIO.read(getClass().getResource("/icons/copy.png"));
+            imagePaste = ImageIO.read(getClass().getResource("/icons/paste.png"));
+            imageCut = ImageIO.read(getClass().getResource("/icons/cut.png"));
+            imageAbout = ImageIO.read(getClass().getResource("/icons/about.png"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert imageNew != null;
+        assert imageOpen != null;
+        assert imageSave != null;
+        assert imageSaveAs != null;
+        assert imageExit != null;
+        assert imageUndo != null;
+        assert imageRedo != null;
+        assert imageCopy != null;
+        assert imagePaste != null;
+        assert imageCut != null;
+        assert imageAbout != null;
+        mnuNewFile.setIcon(new ImageIcon(imageNew));
+        mnuOpenFile.setIcon(new ImageIcon(imageOpen));
+        mnuSaveFile.setIcon(new ImageIcon(imageSave));
+        mnuSaveFileAs.setIcon(new ImageIcon(imageSaveAs));
+        mnuExit.setIcon(new ImageIcon(imageExit));
+        mnuUndo.setIcon(new ImageIcon(imageUndo));
+        mnuRedo.setIcon(new ImageIcon(imageRedo));
+        mnuCopy.setIcon(new ImageIcon(imageCopy));
+        mnuPaste.setIcon(new ImageIcon(imagePaste));
+        mnuCut.setIcon(new ImageIcon(imageCut));
+        mnuHelp.setIcon(new ImageIcon(imageAbout));
+
         return toolbar;
     }
 
@@ -172,6 +212,32 @@ public class Toolbar extends JFrame {
         JOptionPane.showMessageDialog(this, "Button clicked !");
     }
 
+    public void mnuOpenListener(ActionEvent event) {
+        JFileChooser choix = new JFileChooser();
+        int retour = choix.showOpenDialog(this);
+        choix.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if(retour==JFileChooser.APPROVE_OPTION){
+            choix.getSelectedFile().getName();
+            choix.getSelectedFile().getAbsolutePath();
+        } else {
+            JOptionPane.showMessageDialog(this, "Aucun fichier choisi !");
+        }
+    }
+
+    public void mnuSaveAsListener(ActionEvent event) {
+        JFileChooser save = new JFileChooser();
+        save.showSaveDialog(this);
+        File f =save.getSelectedFile();
+        try {
+            FileWriter fw = new FileWriter(f);
+            String text = "Le fichier a été sauvegardé";
+            fw.write(text);
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 
     // Main pour test a part
     public static void main(String[] args) {
